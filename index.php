fc21348a8cc5bc234fae67fd5c4fe904511126e9
Rohit <rohitmpaul@gmail.com>
2019-11-05 15:57:33 +0530

    ForntEnd Front Page

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+ <head>
+     <link rel="stylesheet" type="text/css" href="styles.css">
+  <title>
+   Excel
+  </title>
+</head>
+<body >
+  <div class="navigation-bar">
+    <div id="navigation container">
+      <img src="imgs\excel 2019.png" alt="logo" height="75" width="150" class="logo">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Competitions</a></li>
+      <li><a href="#">Workshops</a></li>
+      <li><a href="#">Talks</a></li>
+      <li><a href="#">Schedule</a></li>
+      <li><a href="#">Contacts</a></li>
+    </ul>
+  </div>
+
+
+
+<p style="font-family:arial" align="center"><b>Excel,the nation's second and south india's first event fest of its kind,was started in 2001 by the students of Govt.Model Engineering College. The aim was simple: to inspire, innovate,engineer.Over the years, Excel has grown exponentially,consistently playing host to some of the most reputed companies.</b></p>
+  <a style="text-decoration:none" href="button3.html">
+<button class="btn btn1">
+    <img src="imgs\event tech.jpg" alt="jelly" height="200" width="300" class="float-img">
+    <p align="left" ><b>Tech Events</b>, Tech Events featured in this list take place all throughout the year, all around the globe. Events featured in the directory cover a number of different industries ,to startup events and more. </p>
+</button>
+</a>
+<a style="text-decoration:none" href="#">
+<button  class="btn btn2">
+    <img src="imgs\non tech.jpg" alt="fresh" height="200" width="300" class="float-img">
+    <p align="left"><b>Non-Tech Events</b>,We thank our departments and other department faculty for arranging fun with in tech fest warm welcome to all weather you are tech or non-tech.Judges are invited from various disciplines and finally judgment was given based on different criteria like combination of different items for decoration, finish with in given time limit, theme.</p>
+</button>
+</a>
+
+</body>
+</html>
