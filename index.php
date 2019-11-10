0e09f4c33415d33a0768efb3ea4c141077a61dd7
Rohit <rohitmpaul@gmail.com>
2019-11-10 17:16:40 +0530

    Update Index.php

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
 <html>
- <head>
-     <link rel="stylesheet" type="text/css" href="styles.css">
-  <title>
-   Excel
-  </title>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<title>ASD Lab</title>
 </head>
-<body >
+
+<body>
+
   <div class="navigation-bar">
-    <div id="navigation container">
-      <img src="imgs\excel 2019.png" alt="logo" height="75" width="150" class="logo">
+    <h class="header"><b>EVENTS BOOKER</b></h>	
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">Competitions</a></li>
-      <li><a href="#">Workshops</a></li>
-      <li><a href="#">Talks</a></li>
-      <li><a href="#">Schedule</a></li>
-      <li><a href="#">Contacts</a></li>
+      <li><a href="#">Login</a></li>
     </ul>
   </div>
 
-<p style="font-family:arial" align="center"><b>Excel,the nation's second and south india's first event fest of its kind,was started in 2001 by the students of Govt.Model Engineering College. The aim was simple: to inspire, innovate,engineer.Over the years, Excel has grown exponentially,consistently playing host to some of the most reputed companies.</b></p>
-  <a style="text-decoration:none" href="button3.html">
-<button class="btn btn1">
-    <img src="imgs\event tech.jpg" alt="jelly" height="200" width="300" class="float-img">
-    <p align="left" ><b>Tech Events</b>, Tech Events featured in this list take place all throughout the year, all around the globe. Events featured in the directory cover a number of different industries ,to startup events and more. </p>
-</button>
-</a>
-<a style="text-decoration:none" href="#">
-<button  class="btn btn2">
-    <img src="imgs\non tech.jpg" alt="fresh" height="200" width="300" class="float-img">
-    <p align="left"><b>Non-Tech Events</b>,We thank our departments and other department faculty for arranging fun with in tech fest warm welcome to all weather you are tech or non-tech.Judges are invited from various disciplines and finally judgment was given based on different criteria like combination of different items for decoration, finish with in given time limit, theme.</p>
-</button>
-</a>
+  <div class="container">
+	
+	<a style="text-decoration:none" href="EventsTech.php">
+		<button class="btn">
+	   		<img src="imgs\tech.png" alt="jelly" height="200" width="300" class="float-img">
+    			<p><b>Tech Events</b></p>
+		</button>
+	</a>
+
+	<a style="text-decoration:none" href="EventsNonTech.php">
+		<button  class="btn">
+		    <img src="imgs\non tech.png" alt="fresh" height="200" width="300" class="float-img">
+		    <p><b>Non-Tech Events</b></p>
+		</button>
+	</a>
+
+  </div>	
 
 </body>
 </html>
