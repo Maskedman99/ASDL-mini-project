d26dbcfc7ab99de841c2cc0e1d2d6f988153d5c0
Rohit <rohitmpaul@gmail.com>
2019-11-10 18:50:47 +0530

    Add Login Page

--- index.php
+++ index.php
@@ -9,11 +9,11 @@
   <div class="navigation-bar">
     <h class="header"><b>EVENTS BOOKER</b></h>	
     <ul>
-      <li><a href="#">Login</a></li>
+     <button onclick="document.getElementById('id01').style.display='block'">Login</button>
     </ul>
   </div>
 
-  <div class="container">
+  <div>
 	
 	<a style="text-decoration:none" href="EventsTech.php">
 		<button class="btn">
@@ -30,6 +30,25 @@
 	</a>
 
   </div>	
+
+<!-- The Login Modal -->
+  <div id="id01" class="modal">
+  	<span onclick="document.getElementById('id01').style.display='none'"
+		class="close" title="Close Modal">&times;</span>
+  <!-- Modal Content -->
+  <form class="modal-content animate" action="login page/login.php">
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <button type="submit">Login</button>
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
+</div>
 
 </body>
 </html>
