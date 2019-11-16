0ebaa9094ff4ffd24520224338e891b5d94ac95f
Rohit <rohitmpaul@gmail.com>
2019-11-16 22:36:34 +0530

    Connect SQL

--- login page/login.php
+++ login page/login.php
@@ -1,8 +1,22 @@
+<?php
+include ($_SERVER['DOCUMENT_ROOT'].'/config.php');
+$conn = OpenConnection();
+?>
+
+<?php
+$names = mysqli_query($conn, "select Name from Admin");
+/*	TO PRINT $names 
+while ($row = $names->fetch_assoc()) {
+    echo $row['Name']."<br>";
+}
+*/
+?>
+ 
 <html>
 <body>
 
 Welcome <?php echo $_GET["uname"]; ?><br>
-Your email address is: <?php echo $_GET["psw"]; ?>
+Your password is: <?php echo $_GET["psw"]; ?>
 
 </body>
 </html> 
